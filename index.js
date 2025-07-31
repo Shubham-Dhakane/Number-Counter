0d06367666bfde1a7da45b79f9dc4161d316c43c
Shubham-Dhakane <dhakane.d23@gmail.com>
2025-07-31 14:29:14 +0530

    new feature added of color

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
 let count = 0; // start from zero
+
+function updateDisplay() {
+    const countElement = document.getElementById("count");
+    countElement.innerText = count;
+
+    // Change color based on value
+    if (count > 0) {
+        countElement.style.color = "green";
+    } else if (count < 0) {
+        countElement.style.color = "red";
+    } else {
+        countElement.style.color = "black";
+    }
+}
 function increase() {
     count++;
     document.getElementById("count").innerText = count;
+    updateDisplay();
 }
 
 function decrease() {
     count--;
     document.getElementById("count").innerText = count;
+    updateDisplay();
 }
 
 function reset() {
     count = 0;
     document.getElementById("count").innerText = count;
+    updateDisplay();
 }
+console.log("Updating count:", count);
