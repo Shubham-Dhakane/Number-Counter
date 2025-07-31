3a342073661928e26af46071cd4cd38e4c4cdc7e
Shubham-Dhakane <dhakane.d23@gmail.com>
2025-07-31 14:56:03 +0530

    added sound when key is pressed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 let count = 0; // start from zero
+const beepSound = new Audio("/button-5.mp3");
 
 function updateDisplay() {
     const countElement = document.getElementById("count");
+     const messageElement = document.getElementById("message");
     countElement.innerText = count;
 
     // Change color based on value
@@ -13,31 +15,41 @@ function updateDisplay() {
     } else {
         countElement.style.color = "black";
     }
+    messageElement.innerText="";  //clear a message on valid keys
 }
 function increase() {
     count++;
     document.getElementById("count").innerText = count;
     updateDisplay();
+    beepSound.play();
 }
 
 function decrease() {
     count--;
     document.getElementById("count").innerText = count;
     updateDisplay();
+    beepSound.play();
 }
 
 function reset() {
     count = 0;
     document.getElementById("count").innerText = count;
     updateDisplay();
+    updateDisplay();
+
 }
 //keyboard shortcut handling 
 document.addEventListener("keydown", function(event){
+    const key=event.key;
+    const messageElement=document.getElementById("message")
     if (event.key==="ArrowUp"){
         increase();
     }else if(event.key ==="ArrowDown"){
         decrease();
     }else if(event.key==="r" || event.key ==="R"){
-        reset()
+        reset();
+    }
+    else {
+        messageElement.innerHTML=`❌ "${key}" is not a valid key. Use ↑, ↓ or R`;
     }
 })
